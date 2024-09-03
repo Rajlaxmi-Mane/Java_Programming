787b29223f1c89aefa665e98f621379a185a7be6
Rajlaxmi-Mane <maithilimanepatil0424@gmail.com>
2024-09-03 21:23:30 +0530

    oops concept

---
+++ OOP/STATIC_MEM/A1.java
@@ -0,0 +1,14 @@
+class A1 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+		test();
+		A1.test();
+	}
+	public static void test()
+	{
+		
+		System.out.println("hii i am from test()");
+	}
+}
