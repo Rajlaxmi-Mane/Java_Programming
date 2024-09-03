199eb80d8146236e17c282366b35d3127d2b4b0e
Rajlaxmi-Mane <maithilimanepatil0424@gmail.com>
2024-09-03 21:07:27 +0530

    java basics

---
+++ BASICS/Book.java
@@ -0,0 +1,16 @@
+class Book 
+{
+	int a=34;
+	public static void main(String[] args) 
+	{
+		int a=10;
+		int b=20;
+		
+		System.out.println(a);
+	}
+	static {
+		int b=30;
+		System.out.println(b);
+	
+	}
+}
---
+++ BASICS/First.java
@@ -0,0 +1,11 @@
+class First 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Helloo itsss mee MAITHILI !!!!!!");
+		int a=10,b=20,c=20;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+	}
+}
